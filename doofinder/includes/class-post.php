b67b222ceaeacfeb2fe41f12cd63d15ad7aa9c27
Sofia Moreno Oñate <sofia@doofinder.com>
2022-05-10 17:38:02 +0200

    apply Pedro suggestion

--- doofinder/includes/class-post.php
+++ doofinder/includes/class-post.php
@@ -348,14 +348,12 @@ class Post {
 	 */
 	private function get_categories() {
 		$all_categories = get_categories();
-		$try_portfolio_cat = get_terms('portfolio_category');
-		$all_portf_categories = (is_wp_error($try_portfolio_cat) ? [] : $try_portfolio_cat);
-		$all_categories = array_merge($all_categories, $all_portf_categories);
-		
 		$post_categories = wp_get_post_categories($this->post->ID, array('fields' => 'all'));
-		$try_portfolio_cat = wp_get_object_terms($this->post->ID, 'portfolio_category', array('fields' => 'all'));
-		$post_portf_categories = (is_wp_error($try_portfolio_cat) ? [] : $try_portfolio_cat);
-		$all_post_categories = array_merge($post_categories, $post_portf_categories);
+
+		if(taxonomy_exists("portfolio_category")){
+			$all_categories = array_merge($all_categories, get_terms('portfolio_category'));
+			$post_categories = array_merge($post_categories, wp_get_object_terms($this->post->ID, 'portfolio_category', array('fields' => 'all')));
+		}
 
 		$formatted_categories = array();
 
@@ -368,7 +366,7 @@ class Post {
 			$categories[ $term->term_id ] = $term;
 		}
 
-		foreach ( $all_post_categories as $category ) {
+		foreach ( $post_categories as $category ) {
 			$formatted_categories[] = join(
 				' > ',
 				array_reverse( $this->get_category_full_path(
