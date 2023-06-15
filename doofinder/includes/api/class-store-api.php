f9fe674b10d79410e84224dd7d77533cf24d6ffc
Pedro <pedro@doofinder.com>
2023-06-15 18:16:16 +0100

    Added normalize_store_and_indices Added application password generation

--- doofinder/includes/api/class-store-api.php
+++ doofinder/includes/api/class-store-api.php
@@ -2,10 +2,14 @@
 
 namespace Doofinder\WP\Api;
 
+use Doofinder\WP\Helpers;
+use Doofinder\WP\Setup_Wizard;
 use Doofinder\WP\Log;
 use Doofinder\WP\Multilanguage\Multilanguage;
+use Doofinder\WP\Multilanguage\No_Language_Plugin;
 use Doofinder\WP\Settings;
 use Exception;
+use WP_Application_Passwords;
 
 defined('ABSPATH') or die();
 
@@ -40,6 +44,13 @@ class Store_Api
      */
     private $api_key;
 
+    /**
+     * APP Credentials option name
+     *
+     * @var string
+     */
+    public static $credentials_option_name = 'doofinder_for_wp_app_credentials';
+
 
     public function __construct()
     {
@@ -65,74 +76,69 @@ class Store_Api
     public function create_store($api_keys)
     {
         if (is_array($api_keys)) {
-            $primary_language = get_locale();
-            if ($this->language->get_languages() != null) {
-                $primary_language = $this->language->get_base_language();
-            }
-            $primary_language = $this->format_language_code($primary_language);
-
-            $domain = str_ireplace('www.', '', parse_url(get_bloginfo('url'), PHP_URL_HOST));
-
-            $store_data = [
-                "name" =>  get_bloginfo('name'),
-                "platform" => "wordpress",
-                "primary_language" => $primary_language,
-                // "skip_indexation" => true,
-                "search_engines" => [],
-                "sector" => Settings::get_sector(),
-                "callback_urls" => [
-                    get_bloginfo('url') . '/wp-json/doofinder/v1/indexation-status/?token=' . $this->api_key
-                ]
-            ];
-
-            if (is_plugin_active('woocommerce/woocommerce.php')) {
-                $product_datatype = $this->get_product_datatype();
-                $currency = get_woocommerce_currency();
-            } else {
-                $product_datatype = [];
-                $currency = "EUR";
-            }
-
-            foreach ($api_keys as $item) {
-                if ($item['hash'] === 'no-hash') {
-                    //Prioritize the locale code
-                    $code = $item['lang']['locale'] ?? $item['lang']['code'] ?? $primary_language;
-                    $code = $this->format_language_code($code);
-                    // Prepare search engine body
-                    $this->log->log('Wizard Step 2 - Prepare Search Enginge body : ');
-
-                    $store_data["search_engines"][] = [
-                        'name' => $domain . ($code ? ' (' . strtoupper($code) . ')' : ''),
-                        'language' => $code,
-                        'currency' => $currency,
-                        'site_url' => get_bloginfo('url'),
-                        'datatypes' => [
-                            [
-                                "name" => "post",
-                                "preset" => "post",
-                                "datasources" => [
-                                    [
-                                        "type" => "wordpress",
-                                        "options" => [
-                                            "feed_type" => "post",
-                                            "url" =>  get_bloginfo('url')
-                                        ]
-                                    ]
-                                ]
-                            ],
-                            $product_datatype
-                        ]
-                    ];
-                }
-            }
-
+            $store_payload = self::build_store_payload($api_keys);
             $this->log->log("store_data: ");
-            $this->log->log($store_data);
-
-            return $this->sendRequest("plugins/create-store", $store_data);
+            $this->log->log($store_payload);
+            return $this->sendRequest("plugins/create-store", $store_payload);
         }
     }
 
+    /**
+     * Sends a request to update the store options with the api password and to create any missing datatype
+     * Payload example:
+     * $payload = array(
+     *    'store_options' => array(
+     *        'url' => 'http://pedro-wordpress.ngrok.doofinder.com',
+     *        'api_pass' => 'G41cXNeVoX4JGL2bhvbcMlQ4',
+     *        'api_user' => 'pedro'
+     *    ),
+     *    'search_engines' => array(
+     *        'fde92a8f364b8d769262974e95d82dba' => array(
+     *            'datatypes' => array(
+     *                array(
+     *                    'name' => 'post',
+     *                    'preset' => 'post',
+     *                    'datasources' => array(
+     *                        array(
+     *                            'type' => 'wordpress',
+     *                            'options' => array(
+     *                                'feed_type' => 'post',
+     *                                'url' => 'http://pedro-wordpress.ngrok.doofinder.com'
+     *    [...]
+     * @return void
+     */
+    public function normalize_store_and_indices()
+    {
+        $wizard = Setup_Wizard::instance();
+        $api_keys = Setup_Wizard::are_api_keys_present($wizard->process_all_languages, $wizard->language);
+        $store_payload = $this->build_store_payload($api_keys);
+
+        $payload = [
+            'store_options' => $store_payload['options']
+        ];
+
+        foreach ($store_payload['search_engines'] as $search_engine) {
+            $lang = Helpers::get_language_from_locale($search_engine['language']);
+
+            //If the installation is not multilanguage, replace the lang with ''
+            if (is_a($this->language, No_Language_Plugin::class)) {
+                $lang = '';
+            }
+
+            $se_hashid = Settings::get_search_engine_hash($lang);
+            $payload['search_engines'][$se_hashid]['datatypes'] = $search_engine['datatypes'];
+        }
+
+        $this->sendRequest("plugins/wordpress/normalize-indices/", $payload);
+    }
+
+    /**
+     * Send a POST request with the given $body to the given $endpoint.
+     *
+     * @param string $endpoint The endpoint url.
+     * @param array $body The array containing the payload to be sent.
+     * @return void
+     */
     private function sendRequest($endpoint, $body)
     {
         $data = [
@@ -144,8 +150,6 @@ class Store_Api
             'method'      => 'POST',
             'data_format' => 'body',
         ];
-
-        $this->api_host = str_replace("https", "http", $this->api_host);
 
         $url = "{$this->api_host}/{$endpoint}";
         $this->log->log("Making a request to: $url");
@@ -161,12 +165,94 @@ class Store_Api
         }
     }
 
-
-    public function format_language_code($code)
+    /**
+     * Generates the create-store payload
+     *
+     * @param array $api_keys The list of search engine ids
+     * @return void
+     */
+    public function build_store_payload($api_keys)
     {
-        return str_replace('_', '-', $code);
+        $primary_language = get_locale();
+        if ($this->language->get_languages() != null) {
+            $primary_language = $this->language->get_base_language();
+        }
+
+        $primary_language = Helpers::format_locale_to_hyphen($primary_language);
+        $domain = str_ireplace('www.', '', parse_url(get_bloginfo('url'), PHP_URL_HOST));
+        $store_options =  self::get_store_options();
+
+        if (is_null($store_options)) {
+            throw new Exception("Invalid store options");
+        }
+
+        $store_payload = [
+            "name" =>  get_bloginfo('name'),
+            "platform" => "wordpress",
+            "primary_language" => $primary_language,
+            // "skip_indexation" => true,
+            "search_engines" => [],
+            "sector" => Settings::get_sector(),
+            "callback_urls" => [
+                get_bloginfo('url') . '/wp-json/doofinder/v1/indexation-status/?token=' . $this->api_key
+            ],
+            "options" => $store_options
+        ];
+
+        if (is_plugin_active('woocommerce/woocommerce.php')) {
+            $product_datatype = $this->get_product_datatype();
+            $currency = get_woocommerce_currency();
+        } else {
+            $product_datatype = [];
+            $currency = "EUR";
+        }
+
+        if (!Multilanguage::$is_multilang) {
+            $api_keys = [
+                '' => [
+                    'hash' => 'no-hash'
+                ]
+            ];
+        }
+
+        foreach ($api_keys as $item) {
+            //Prioritize the locale code
+            $code = $item['lang']['locale'] ?? $item['lang']['code'] ?? $primary_language;
+            $code = Helpers::format_locale_to_hyphen($code);
+
+            // Prepare search engine body
+            $this->log->log('Wizard Step 2 - Prepare Search Enginge body : ');
+            $store_payload["search_engines"][] = [
+                'name' => $domain . ($code ? ' (' . strtoupper($code) . ')' : ''),
+                'language' => $code,
+                'currency' => $currency,
+                'site_url' => get_bloginfo('url'),
+                'datatypes' => [
+                    [
+                        "name" => "post",
+                        "preset" => "post",
+                        "datasources" => [
+                            [
+                                "type" => "wordpress",
+                                "options" => [
+                                    "feed_type" => "post",
+                                    "url" =>  get_bloginfo('url')
+                                ]
+                            ]
+                        ]
+                    ],
+                    $product_datatype
+                ]
+            ];
+        }
+        return $store_payload;
     }
 
+    /**
+     * Generates the product datatype structure.
+     *
+     * @return array The product datatype structure.
+     */
     public function get_product_datatype()
     {
         return [
@@ -182,5 +268,70 @@ class Store_Api
                 ]
             ]
         ];
+    }
+
+    /**
+     * Generates an api_password and returns the store options.
+     *
+     * @return void
+     */
+    private static function get_store_options()
+    {
+        $password_data = self::create_application_credentials();
+        if (!is_null($password_data)) {
+            return [
+                "url" => get_bloginfo('url'),
+                'api_pass' => $password_data['api_pass'],
+                'api_user' => $password_data['api_user']
+            ];
+        }
+        return NULL;
+    }
+
+    /**
+     * This method checks if there is an application password set.
+     *
+     * @return boolean
+     */
+    public static function has_application_credentials()
+    {
+        return WP_Application_Passwords::application_name_exists_for_user(get_current_user_id(), 'doofinder');
+    }
+
+    /**
+     * Creates a new application password.
+     * If a password exists, it deletes it and creates a new password.
+     *
+     * We store the user_id and the uuid in order to know which application
+     * password we must delete.
+     *
+     * @return array Array containing api_user and api_pass
+     */
+    public static function create_application_credentials()
+    {
+        $user_id = get_current_user_id();
+        $user = get_user_by('id',  $user_id);
+        $credentials = get_option(self::$credentials_option_name);
+        $password_data = NULL;
+
+        if (is_array($credentials) && array_key_exists('user_id', $credentials) &&  array_key_exists('uuid', $credentials)) {
+            WP_Application_Passwords::delete_application_password($credentials['user_id'], $credentials['uuid']);
+        }
+
+        if (!WP_Application_Passwords::application_name_exists_for_user($user_id, 'doofinder')) {
+            $app_pass = WP_Application_Passwords::create_new_application_password($user_id, array('name' => 'doofinder'));
+            $credentials = [
+                'user_id' => $user_id,
+                'uuid' => $app_pass[1]['uuid']
+            ];
+
+            update_option(self::$credentials_option_name, $credentials);
+
+            $password_data = [
+                'api_user' => $user->data->user_login,
+                'api_pass' => $app_pass[0]
+            ];
+        }
+        return $password_data;
     }
 }
