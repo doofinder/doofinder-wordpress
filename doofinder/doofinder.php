df5bd3c6f469d2c6ff61fdd047593d231fc19dd8
Pedro <pedro@doofinder.com>
2023-07-05 16:03:51 +0100

    Changed Doofinder for woocommerce deprecated text

--- doofinder/doofinder.php
+++ doofinder/doofinder.php
@@ -132,7 +132,7 @@ if (!class_exists('\Doofinder\WP\Doofinder_For_WordPress')) :
                 //Check if the plugin exists
                 $old_plugin_notice_name = 'doofinder-for-wp-old-version-detected';
                 if (file_exists(WP_PLUGIN_DIR . '/doofinder-for-woocommerce/doofinder-for-woocommerce.php')) {
-                    Admin_Notices::add_notice($old_plugin_notice_name, __('Deprecated version of Doofinder plugin detected', 'wordpress-doofinder'), __('The Doofinder plugin has been merged into the new version of Doofinder for WooCommerce and is no longer needed. Therefore, we have deactivated it. We recommend uninstalling it to avoid future issues.', 'wordpress-doofinder'), 'warning');
+                    Admin_Notices::add_notice($old_plugin_notice_name, __('Deprecated version of Doofinder for WooCommerce plugin detected', 'wordpress-doofinder'), __('The "Doofinder for WooCommerce" plugin has been merged into the new version of Doofinder and is no longer needed. Therefore, we have deactivated it. We recommend uninstalling it to avoid future issues.', 'wordpress-doofinder'), 'warning');
                 } else {
                     Admin_Notices::remove_notice($old_plugin_notice_name);
                 }
