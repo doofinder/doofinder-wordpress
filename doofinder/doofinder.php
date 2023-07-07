34e9567c011a2aca3bac060d9c0233c65819fe7c
Pedro <pedro@doofinder.com>
2023-07-07 17:08:15 +0100

    - Fixed some setup-wizard issues - Enable layer after indexing finished

--- doofinder/doofinder.php
+++ doofinder/doofinder.php
@@ -329,7 +329,7 @@ if (!class_exists('\Doofinder\WP\Doofinder_For_WordPress')) :
             add_action('rest_api_init', function () {
                 Config::register();
                 register_rest_route('doofinder/v1', '/index-status', array(
-                    'methods' => 'POST',
+                    'methods' => ['POST', 'GET'],
                     'callback' => function (\WP_REST_Request $request) {
                         $log = new Log('index-status.log');
                         $log->log("Received indexing status request with payload:\n" . print_r($request, true));
@@ -344,7 +344,8 @@ if (!class_exists('\Doofinder\WP\Doofinder_For_WordPress')) :
                             );
                         }
 
-                        if ($error_message = $request->get_param('message') != $valid_message) {
+                        $error_message = $request->get_param('message');
+                        if (!empty($error_message) && $error_message != $valid_message) {
                             Setup_Wizard::dismiss_indexing_notice();
                             Admin_Notices::add_notice("indexing-status-failed", __("The indexation failed", "wordpress-doofinder"), $error_message, 'error', null, '', true);
 
@@ -363,6 +364,8 @@ if (!class_exists('\Doofinder\WP\Doofinder_For_WordPress')) :
                         //Hide the indexing notice
                         Setup_Wizard::dismiss_indexing_notice();
                         Settings::set_indexing_status('processed', $lang);
+                        // Enable JS Layer for the indexed language
+                        Settings::enable_js_layer($lang);
 
                         return new WP_REST_Response(
                             [
